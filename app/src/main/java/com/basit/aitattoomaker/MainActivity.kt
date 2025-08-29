195c194f38abf42ae5ae0bf43c8d79ff56aacd06
abdulbasit <abasit@terafort.com>
2025-08-29 11:40:54 +0500

    setting screen continue

--- app/src/main/java/com/basit/aitattoomaker/MainActivity.kt
+++ app/src/main/java/com/basit/aitattoomaker/MainActivity.kt
@@ -23,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         this.hideSystemBars()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding?.navView?.itemBackground = null
+
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
